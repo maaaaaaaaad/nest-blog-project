e6b2d169b0ba4a05e91cb398e5e1627682ed7ea9
DONG WOONG OH <integral5137@gmail.com>
2021-07-14 00:46:43 +0900

    Add to Global Pipes and modify blog-data dto

--- src/container/dto/blog-data.dto.ts
+++ src/container/dto/blog-data.dto.ts
@@ -1,7 +1,18 @@
+import { IsString } from 'class-validator';
+
 export class CreatePostDTO {
+  @IsString()
   readonly title: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString()
   readonly body: string;
+
+  @IsString()
   readonly author: string;
+
+  @IsString()
   readonly date_posted: string;
 }

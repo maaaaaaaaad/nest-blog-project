af8a63429b23608beb76cd5e2811531bf321cd2a
DONG WOONG OH <integral5137@gmail.com>
2021-07-16 06:27:25 +0900

    Solved cors issue

--- src/container/container.module.ts
+++ src/container/container.module.ts
@@ -1,14 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ContainerService } from './container.service';
 import { ContainerController } from './container.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { blogDataSchema } from './schemas/blog-data.schema';
-import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
   imports: [
@@ -20,22 +14,4 @@ import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
   controllers: [ContainerController],
 })
 //
-export class ContainerModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
-    //
-    consumer.apply(AuthenticationMiddleware).forRoutes(
-      {
-        method: RequestMethod.POST,
-        path: '/container/post',
-      },
-      {
-        method: RequestMethod.PATCH,
-        path: '/container/post/update',
-      },
-      {
-        method: RequestMethod.DELETE,
-        path: '/container/post/delete',
-      },
-    );
-  }
-}
+export class ContainerModule {}
